32816c5413e62cdac38bc57686a23dd04cfb6c58
caleb-copeland <caleb@calebacopeland.com>
2020-12-21 12:35:47 -0500

    12.21.20-12:35pm
    Resize bugs fixed

--- javascript/better-masonry.js
+++ javascript/better-masonry.js
@@ -1,83 +1,123 @@
+/*------------------------------------*/
+/*---------- Better Masonry ----------*/
+/*------------------------------------*/
+let screenWidth = $(window).width(); // Variable to hold current screen width
+let layoutChange = false;
+let cardPositions = [];
+let cardHeights = []; //Array stores height of every ".portfolio-card"
+const padding = (16 * 2); // 16px (1rem @ lg screen)
+
 $(document).ready(function() {
   /*---------- Set layout when page is loaded ----------*/
-  let screenWidth = $(window).width(); // Variable to hold current screen width
   const largeScreen = 992; // Width of large screen break (px)
   const xtraLargeScreen = 1200; // Width of x-large screen break (px)
 
-  $(document).ready(function() {
-    // If screen width is greater than "largeScreen"...
-    if (screenWidth >= largeScreen) {
-      updateDesktopLayout();
-      // If screen width is smaller than "largeScreen"...
-    } else if (screenWidth < largeScreen) {
-      updateMobileLayout();
-    }
-  })
+  /*---------- When the page loads run the function onLoad ----------*/
+  // If screen width is greater than "largeScreen"...
+  if (screenWidth >= largeScreen) {
+    updateDesktopLayout();
+    currentLayout = 'desktop';
+    // If screen width is smaller than "largeScreen"...
+  } else if (screenWidth < largeScreen) {
+    updateMobileLayout();
+    currentLayout = 'mobile';
+  }
 
   /*---------- When the window is resized ----------*/
-  $(window).resize(function() {
-    // If the window has been resized to a "largeScreen" OR an "extraLargeScreen" width...
-    if ((screenWidth >= xtraLargeScreen && $(this).width() < xtraLargeScreen) || (screenWidth < xtraLargeScreen && $(this).width() >= xtraLargeScreen) || (screenWidth < largeScreen && $(this).width() >= largeScreen)) {
-      screenWidth = $(this).width();
-      updateDesktopLayout();
+  $(window).on('resize keydown', function() {
+    /*---------- If the window has been resized from an "XL" screen to a "largeScreen" width ----------*/
+    if (screenWidth >= xtraLargeScreen && ($(window).width() < xtraLargeScreen && $(window).width() >= largeScreen)) {
+      console.log('XL -> L');
+      layoutUpdater('desktop');
+
+      /*---------- If the window has been resized from a "largeScreen" screen to an "XL" width ----------*/
+    } else if ((screenWidth >= largeScreen && screenWidth < xtraLargeScreen) && ($(window).width() >= xtraLargeScreen)) {
+      console.log('L -> XL');
+      layoutUpdater('desktop');
+
+      /*---------- If the window has been resized from a "Mobile" screen to a "largeScreen" width ----------*/
+    } else if (screenWidth < largeScreen && ($(window).width() >= largeScreen && $(window).width() < xtraLargeScreen)) {
+      console.log('M -> L');
+      layoutUpdater('desktop');
+
+      /*---------- If the window has been resized from a "Mobile" screen to an "xtraLargeScreen" width ----------*/
+    } else if ((screenWidth < largeScreen) && ($(window).width() >= xtraLargeScreen)) {
+      console.log('M -> XL');
+      layoutUpdater('desktop');
+
       // If the window has been resized to a width smaller than "largeScreen" width...
-    } else if (screenWidth >= largeScreen && $(this).width() < largeScreen) {
-      screenWidth = $(this).width();
-      updateMobileLayout();
+    } else if (screenWidth >= largeScreen && $(window).width() < largeScreen) {
+      console.log('L -> mobile');
+      layoutUpdater('mobile');
+
+    }
+
+    /*---------- If a layout change has been activated... ----------*/
+    if (layoutChange) {
+      $('.free-masonry').css('opacity', 0); // All elements in class to opacity:0
+      layoutChange = false; // Reset 'layoutChange'
     }
   })
+})
 
-  /*---------- Function to update the layout in desktop view ----------*/
-  const updateDesktopLayout = () => {
-    var cardPositions = [];
-    var cardHeights = []; //Array stores height of every ".portfolio-card"
-    var padding = (16 * 2); // 16px (1rem @ lg screen)
 
-    // Loop through every instance of ".free-masonty"...
+/*---------- Selects which layout to update, updates "screenWidth", and initiates timeouts  ----------*/
+const layoutUpdater = (layout) => {
+  layoutChange = true;
+  screenWidth = $(window).width();
+  console.log(screenWidth);
+
+  setTimeout(function() {
+    if (layout === 'desktop') {
+      updateDesktopLayout();
+    } else if (layout === 'mobile') {
+      updateMobileLayout();
+    }
+  }, 300);
+}
+
+
+/*---------- Function to update the layout in desktop view ----------*/
+const updateDesktopLayout = () => {
+  let heightGap = 0; // Initialize heightGap variable...
+  $('.free-masonry').css('top', 0); // Resets the CSS 'top' value
+
+  setTimeout(function() {
+    // Loop through every instance of ".free-masonry"...
     $('.free-masonry').each(function(i) {
-      cardPositions[i] = $(this).offset(); // Stores all card top positions in array
       cardHeights[i] = $(this).outerHeight(); // Store all card heights in array (including margins)
 
-      // If the current card's index IS AFTER the first two cards...
-      // [The first two cards will always be flush with the top]
-      if (i > 1) {
-        var currentCardTop = cardPositions[i].top; // Get TOP position of current card
+      if ((i > 1) && (i % 2 === 0)) {
+        heightGap += cardHeights[i - 1] - cardHeights[i - 2]; // Variable to hold how far the card needs to be adjusted.
+        $(this).css('top', padding - heightGap);
 
-        var cardDirectlyAboveTop = cardPositions[i - 2].top; // Get TOP position of the card that is directly above the current card
-
-        var cardDirectlyAboveHeight = cardHeights[i - 2]; // Get HEIGHT of the card that is directly above the current card
-        var cardDirectlyAboveBottom = cardDirectlyAboveTop + cardDirectlyAboveHeight; // Get BOTTOM position of the card that is directly above the current card
-
-        var heightGap = currentCardTop - cardDirectlyAboveBottom; // Variable to hold how far the card needs to be adjusted.
-
-        var updatedCurrentCardTop = currentCardTop - heightGap; // Variable holds the card's current top position minus the distance to the bottom of the card directly above it.
-
-        $(this).offset({
-          top: updatedCurrentCardTop + padding
-        }); // Adjust the actual top position in the current card
-
-        cardPositions[i].top = $(this).offset().top; // Update the position array with the new top position for the current card. This affects the placement of the upcoming cards in the loop (if there are any)
-
-        /*---------- Console Logs ----------*/
-        // console.log('-------------------------');
-        // console.log(`Above Top: ${cardDirectlyAboveTop}`);
-        // console.log(`Above Height: ${cardDirectlyAboveHeight}`);
-        // console.log(`Above Bottom: ${cardDirectlyAboveBottom}`);
-        // console.log(`Curent Top: ${currentCardTop}`);
-        // console.log(`Height Gap: ${heightGap}`);
-        // console.log(`New Current Top: ${updatedCurrentCardTop}`);
-        // console.log(`Adjusted Top: ${cardPositions[i].top}`);
-
+      } else if ((i > 1) && (i % 2 !== 0)) {
+        $(this).css('top', padding);
       }
-    });
-  }
+    })
+    fadeInClass('.free-masonry');
+  }, 500);
+}
 
-  /*---------- Function to update the layout in mobile view ----------*/
-  const updateMobileLayout = () => {
-    $('.free-masonry').each(function(i) {
-      $(this).css('top', '0px');
-      $(this).css('margin-top', '0.75rem');
-    });
-  }
+/*---------- Function to update the layout in mobile view ----------*/
+const updateMobileLayout = () => {
+  setTimeout(function() {
+    $('.free-masonry').each(function() {
+      /*---------- A full reset to return to 'Bootstrap' standards ----------*/
+      $(this).css('top', padding);
+      var top = $(this).css('top');
+      console.log(top);
+    })
 
-})
+    fadeInClass('.free-masonry');
+  }, 500);
+}
+
+/*---------- Fade In all elements of class '.free-masonry' simultaneously  ----------*/
+const fadeInClass = (className) => {
+  setTimeout(function () {
+    $(className).animate({
+      opacity: 1
+    }, 'slow')
+  }, 250);
+}
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -16,16 +16,18 @@ $(document).ready(function() {
 
   /*---------- FADE-OUT the page when a link is clicked ----------*/
   $('a').click(function(e) {
+    /*---------- If the user clicked a link that has the '.fade-js' class... ----------*/
     if ($(this).hasClass('fade-link-js')) {
+      /*---------- If the link is ALSO in the navbar... ----------*/
       if ($(this).hasClass('nav-link')) {
         $(this).attr('class', 'nav-link active fade-link-js')
       }
-      e.preventDefault();
-      $('body').addClass('fade-out');
+      e.preventDefault(); // Prevent the link from activating right away
+      $('body').addClass('fade-out'); // Fade out the entire page
       var newLocation = this.href;
 
       setTimeout(function() {
-        window.location.href = newLocation;
+        window.location.href = newLocation; // Go to the links href location in 0.5s
       }, 500);
     }
   })
